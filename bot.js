e570171620198fc9f4ef926c91a0fd71a0f83d11
Taras Yevtushenko <taras.evt@gmail.com>
2017-05-25 11:59:19 +0300

    Initial commit

---
+++ bot.js
@@ -0,0 +1,29 @@
+const TeleBot = require('telebot');
+const config = require('./config.json');
+
+const bot = new TeleBot({
+    token: config.token, // Required. Telegram Bot API token.
+    webhook: {
+        url: config.webhookUrl,
+        host: '127.0.0.1',
+        port: '8083'
+    }
+});
+
+bot.on(/^\/say(@xxlv_bot)? (.+)$/, (msg, props) => {
+    const text = props.match[2];
+    let arr = text.split(' ');
+    if (arr.length > 5) {
+        arr = arr.slice(0, 5).concat(['...']);
+    }
+
+    arr.reduce((def, str) => {
+        return def.then(() => {
+            bot.sendMessage(msg.chat.id, str);
+
+            return new Promise(resolve => setTimeout(() => resolve(), 1000));
+        });
+    }, Promise.resolve());
+});
+
+bot.start();
