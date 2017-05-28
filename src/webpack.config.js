bdf5ecd811fd836741360bcd97a32e186f359289
Taras Yevtushenko <taras.evt@gmail.com>
2017-05-28 13:37:07 +0300

    Initial webpack config for game

---
+++ src/webpack.config.js
@@ -0,0 +1,47 @@
+const { resolve } = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = function(env) {
+    const filename = env.production ? '[hash].bundle.js' : 'bundle.js';
+    const devtool = env.production ? false : 'cheap-eval-source-map';
+    const plugins = [
+        new HtmlWebpackPlugin({
+            template: 'index.ejs',
+            filename: 'index.html'
+        }),
+        new CleanWebpackPlugin(['public'], {
+            root: resolve(__dirname, '../')
+        })
+    ];
+
+    if (env.production) {
+        plugins.push(
+            new webpack.optimize.UglifyJsPlugin({
+                compress: false
+            })
+        );
+    }
+
+    const conf = {
+        entry: ['./game.js'],
+        output: {
+            filename,
+            path: resolve(__dirname, '../public')
+        },
+        devtool,
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader'
+                }
+            ]
+        },
+        plugins
+    };
+
+    return conf;
+};
